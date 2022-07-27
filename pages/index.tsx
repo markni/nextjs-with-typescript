1918b5c3f48708badb768256b72041063ae7f8e8
mark <xixian.ni@gmail.com>
2022-07-27 11:09:28 -0700

    adding more typography

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,29 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '../src/Link';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
-import {Button, Fab} from "@mui/material";
+
+import {Button, Grid, TextField} from "@mui/material";
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 const Home: NextPage = () => {
+    const [open, setOpen] = React.useState(false);
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
   return (
     <Container maxWidth="xs">
       <Box
@@ -18,23 +36,25 @@ const Home: NextPage = () => {
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
-            gap: '20px'
+            gap: '8px'
         }}
       >
-        <Typography variant="subtitle1" component="h4" gutterBottom>
+        <Typography variant="subtitle1" >
           MUI v5 + Next.js with TypeScript example
         </Typography>
 
 
-          <Typography variant={"h5"}  >
+          <Typography variant={"h2"}  >
               Combined Next + Mui Links
           </Typography>
 
           <Link href="/about" color="secondary">
-              Go to the about page
+              <Typography  >
+                  Just a link
+              </Typography>
           </Link>
 
-          <Typography variant={"h5"}  >
+          <Typography variant={"h2"}  >
             Full Width Buttons
           </Typography>
           <Button variant="contained" disableElevation color={'primary'} fullWidth>Primary</Button>
@@ -42,19 +62,95 @@ const Home: NextPage = () => {
           <Button variant="contained" disableElevation color={'primary'} disabled fullWidth>Disabled</Button>
           <Button variant="contained" disableElevation color={'tertiary'} fullWidth>Tertiary (custom color)</Button>
 
-          <Typography variant={"h5"}  >
+          <Typography variant={"h2"}  >
               Small Buttons
           </Typography>
 
           <Button variant="contained" disableElevation color={'tertiary'} >Apply</Button>
 
-          <Typography variant={"h5"}  >
-              Typography
-          </Typography>
 
           <Typography >
               Regular Text
           </Typography>
+
+          <Typography color={'secondary'} >
+              Regular Text with secondary color
+          </Typography>
+
+          <Typography variant={'highlight'}  >
+              Highlight variant (used to display discount in design)
+          </Typography>
+
+          <Typography color={'highlight.main'}>
+              Just the highlight color
+          </Typography>
+
+          <Typography variant={'total'} >
+              Total: $30.00
+          </Typography>
+
+          <Typography variant={'h2'} >
+              Header 2 (used on left top of each page)
+          </Typography>
+
+          <Typography variant={'subtitle1'} >
+              Subtitle 1 (used on left top of each page)
+          </Typography>
+
+          <Typography variant={'subtitle2'} >
+              Subtitle 2 (used in two column design, title for each sections)
+          </Typography>
+
+          <Typography variant={'hint'} >
+              Hint (small grey text, used to display brand name etc)
+          </Typography>
+
+
+          <Typography color={'error'} variant={'button'} >
+              Delete address
+          </Typography>
+
+          <Link href="/about" color="secondary">
+
+          <Typography color={'secondary'} variant={'button'} >
+              Add discount
+          </Typography>
+          </Link>
+
+
+
+          <Button variant="outlined" onClick={handleClickOpen}>
+              Responsive dialog example
+          </Button>
+          <Dialog
+              fullScreen={fullScreen}
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="responsive-dialog-title"
+          >
+              <DialogTitle id="responsive-dialog-title">
+                  <Typography variant={'h2'}>New Address</Typography>
+              </DialogTitle>
+              <DialogContent>
+                  <TextField fullWidth label="First Name" variant="outlined" placeholder={'First name'} />
+                  <TextField fullWidth label="First Name" variant="outlined" placeholder={'First name'} />
+
+              </DialogContent>
+              <DialogActions>
+                  <Grid container spacing={2} xs={6}>
+                      <Grid item xs={4}>
+                          <Button fullWidth disableElevation variant="outlined" autoFocus onClick={handleClose}>
+                              Cancel
+                          </Button>
+                      </Grid>
+                      <Grid item xs={8}>
+                          <Button fullWidth disableElevation  variant="contained" onClick={handleClose} autoFocus>
+                              Save
+                          </Button>
+                      </Grid>
+                  </Grid>
+              </DialogActions>
+          </Dialog>
 
       </Box>
     </Container>
--- src/theme.ts
+++ src/theme.ts
@@ -1,19 +1,46 @@
 import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import React from "react";
 
 declare module '@mui/material/styles/createPalette' {
   interface Palette {
     tertiary: {main: string, contrastText: string};
+    highlight: {main: string};
   }
 
   interface PaletteOptions {
     tertiary?: {main: string, contrastText: string};
+    highlight: {main: string};
   }
 }
 
 declare module "@mui/material" {
   interface ButtonPropsColorOverrides {
     tertiary: true;
+    highlight: true;
+  }
+}
+
+declare module '@mui/material/styles' {
+  interface TypographyVariants {
+    total: React.CSSProperties;
+    highlight: React.CSSProperties;
+    hint: React.CSSProperties;
+  }
+
+  // allow configuration using `createTheme`
+  interface TypographyVariantsOptions {
+    total?: React.CSSProperties;
+    highlight?: React.CSSProperties;
+    hint?: React.CSSProperties;
+  }
+}
+
+// Update the Typography's variant prop options
+declare module '@mui/material/Typography' {
+  interface TypographyPropsVariantOverrides {
+    total: true;
+    highlight: true;
+    hint: true;
   }
 }
 
@@ -21,6 +48,24 @@ declare module "@mui/material" {
 // Create a theme instance.
 const theme = createTheme({
   components: {
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          subtitle1: 'div', // https://css-tricks.com/html-for-subheadings-and-headings/
+          subtitle2: 'div',
+          body1: 'span',
+          body2: 'span',
+        },
+      },
+    },
+    MuiDialog: {
+      styleOverrides: {
+        paper: {
+          borderRadius: '12px'
+        },
+
+      }
+    },
     MuiButton: {
       styleOverrides: {
         root: {
@@ -31,6 +76,7 @@ const theme = createTheme({
           fontWeight: '700',
           lineHeight: '20px',
           padding: '9px 20.5px',
+          textDecoration: 'none',
           ":disabled": {
             color: '#FFFFFF',
             background: '#B2B2B2'
@@ -41,20 +87,72 @@ const theme = createTheme({
   },
   palette: {
     primary: {
-      main: '#000000',
+      main: '#333000',
     },
     secondary: {
       main: '#1F7F79',
-
     },
-    tertiary: {
+
+    tertiary: { //color for third button style
       main: '#E2F7F2',
       contrastText: '#000000',
-    }
+    },
 
+    highlight: {
+      main: '#4EAE57',
+    },
   },
   typography: {
     fontFamily: 'Poppins',
+    h1: undefined,
+    h2: { // use for title of each page, we only use h3 for now
+      fontWeight: 700,
+      fontSize: 18,
+      lineHeight: 1.5,
+      marginTop: '8px'
+    },
+    h3: undefined,
+    h4: undefined,
+    h5: undefined,
+    h6: undefined,
+    button: {
+      textTransform: 'none',
+      fontWeight: 700,
+      lineHeight: 1.5,
+      textDecoration: 'underline',
+      cursor: 'pointer',
+      fontSize: 14,
+    },
+    body1: {
+      fontSize: 14,
+    },
+    subtitle1: { //the subtitle above h2
+      fontSize: 12,
+      fontWeight: 400,
+      lineHeight: 1,
+    },
+    subtitle2: { //row titles, section titles
+      fontSize: 14,
+      lineHeight: 1.5,
+      color: '#757575',
+      fontWeight: 500,
+    },
+    hint: {
+      fontWeight: 400,
+      fontSize: 12,
+      color: '#979797',
+      lineHeight: 1.5
+    },
+    total: {
+      fontWeight: 700,
+      fontSize: 18,
+      lineHeight: 1.5
+    },
+    highlight: {
+      fontWeight: 600,
+      fontSize: 14,
+      color: '#4EAE57',
+    }
   },
 });
 
